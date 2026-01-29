09174feca546733cc913dfc957891489bf06426d
Adeonir Kohl <adeonir@gmail.com>
2026-01-29 08:09:51 -0300

    chore: add VITE_SITE_URL environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
+// biome-ignore-all lint/correctness/noUnusedVariables: Vite env type augmentation
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_APP_NAME: string
+  readonly VITE_SITE_URL: string
   readonly VITE_POSTHOG_KEY: string
   readonly VITE_POSTHOG_HOST: string
 }
